d964a445d1e3300e78e11c4f316518dfdc0fe8be
Gummibeer <dev.gummibeer@gmail.com>
2019-05-02 10:51:32 +0200

    update header

--- lss_automator.user.js
+++ lss_automator.user.js
@@ -1,3 +1,14 @@
+// ==UserScript==
+// @name        Script für Leitstellenspiel
+// @namespace   Leitstellenspiel
+// @include     https://www.leitstellenspiel.de
+// @version     0.1.2
+// @author      Gummibeer
+// @grant       none
+// @updateURL   https://raw.githubusercontent.com/Gummibeer/lss_automator/master/lss_automator.user.js
+// @downloadURL https://raw.githubusercontent.com/Gummibeer/lss_automator/master/lss_automator.user.js
+// ==/UserScript==
+
 (function() {
     console.log('init LSS-Automator');
 
