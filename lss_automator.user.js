91ff302f3044f735d618ea218efff452d47ec47c
Gummibeer <dev.gummibeer@gmail.com>
2019-05-02 11:49:00 +0200

    only log mission events on main page

--- lss_automator.user.js
+++ lss_automator.user.js
@@ -3,7 +3,7 @@
 // @description A userscript that automates missions
 // @namespace   https://www.leitstellenspiel.de
 // @include     https://www.leitstellenspiel.de/*
-// @version     0.1.3
+// @version     0.1.4
 // @author      Gummibeer
 // @license     MIT
 // @run-at      document-end
@@ -11,19 +11,22 @@
 // ==/UserScript==
 
 (function() {
+    if (window.location.href !== 'https://www.leitstellenspiel.de/') {
+        return;
+    }
+
     console.log('init LSS-Automator');
 
     const subscription = faye.subscribe('/private-user'+user_id+'de', handleFayeEvent );
 
     function handleFayeEvent(message) {
+        if(!(
+            message.indexOf('missionMarkerAdd') === 0
+            || message.indexOf('missionDelete') === 0
+        )) {
+            return;
+        }
+
         console.log(message);
     }
 })();
-
-$(window).load(function(){
-    console.log('window.load');
-});
-
-$(document).ready(function(){
-    console.log('document.ready');
-});
