77d0aa520579db9956513b832426367e1057743e
Gummibeer <dev.gummibeer@gmail.com>
2019-05-04 15:06:07 +0200

    add logger

---
+++ logger.js
@@ -0,0 +1,141 @@
+class Logger {
+    static get DEBUG() {
+        return 100;
+    };
+
+    static get INFO() {
+        return 200;
+    };
+
+    static get NOTICE() {
+        return 250;
+    };
+
+    static get WARNING() {
+        return 300;
+    };
+
+    static get ERROR() {
+        return 400;
+    };
+
+    static get CRITICAL() {
+        return 500;
+    };
+
+    static get ALERT() {
+        return 550;
+    };
+
+    static get EMERGENCY() {
+        return 600;
+    };
+
+    static get LEVELS() {
+        return {
+            'debug': this.DEBUG,
+            'info': this.INFO,
+            'notice': this.NOTICE,
+            'warning': this.WARNING,
+            'error': this.ERROR,
+            'critical': this.CRITICAL,
+            'alert': this.ALERT,
+            'emergency': this.EMERGENCY,
+        }
+    };
+
+    static get STYLES() {
+        return {
+            [this.DEBUG]: 'color: #9A9A9A',
+            [this.INFO]: 'color: #212121',
+            [this.NOTICE]: 'color: #325A85',
+            [this.WARNING]: 'color: #7A6223',
+            [this.ERROR]: 'color: #EC3D47',
+            [this.CRITICAL]: 'color: #AD2D33',
+            [this.ALERT]: 'color: #6E1D21',
+            [this.EMERGENCY]: 'color: #6E1D21',
+        }
+    };
+
+    static get METHODS() {
+        return {
+            [this.DEBUG]: console.debug ? 'debug' : 'log',
+            [this.INFO]: console.info ? 'info' : 'log',
+            [this.NOTICE]: console.info ? 'info' : 'log',
+            [this.WARNING]: console.warn ? 'warn' : 'log',
+            [this.ERROR]: console.error ? 'error' : 'log',
+            [this.CRITICAL]: console.error ? 'error' : 'log',
+            [this.ALERT]: console.error ? 'error' : 'log',
+            [this.EMERGENCY]: console.error ? 'error' : 'log',
+        }
+    };
+
+    constructor(module, level, formatter) {
+        this._module = String(module);
+        this._level = this._normalizeLevel(level, Logger.ERROR);
+        this._formatter = typeof formatter === 'function' ? formatter : function (level, message) {
+            return '[' + this._module + '][' + level + '] ' + message;
+        };
+    }
+
+    log(level, message) {
+        level = this._normalizeLevel(level, Logger.INFO);
+
+        if (level < this._level) {
+            return;
+        }
+
+        let method = Logger.METHODS[level];
+
+        console[method]('%c' + this._formatter.call(this, level, message), Logger.STYLES[level]);
+    }
+
+    debug(message) {
+        this.log(Logger.DEBUG, message);
+    }
+
+    info(message) {
+        this.log(Logger.INFO, message);
+    }
+
+    notice(message) {
+        this.log(Logger.NOTICE, message);
+    }
+
+    warning(message) {
+        this.log(Logger.WARNING, message);
+    }
+
+    error(message) {
+        this.log(Logger.ERROR, message);
+    }
+
+    critical(message) {
+        this.log(Logger.CRITICAL, message);
+    }
+
+    alert(message) {
+        this.log(Logger.ALERT, message);
+    }
+
+    emergency(message) {
+        this.log(Logger.EMERGENCY, message);
+    }
+
+    _normalizeLevel(level, fallback) {
+        if (
+            typeof level === 'number'
+            && Object.values(Logger.LEVELS).indexOf(level) !== -1
+        ) {
+            return level;
+        } else if (
+            typeof level === 'string'
+            && Object.keys(Logger.LEVELS).indexOf(level.toLowerCase()) !== -1
+        ) {
+            return Logger.LEVELS[level.toLowerCase()];
+        }
+
+        return fallback;
+    }
+}
+
--- lss_automator.user.js
+++ lss_automator.user.js
@@ -3,18 +3,20 @@
 // @description A userscript that automates missions
 // @namespace   https://www.leitstellenspiel.de
 // @include     https://www.leitstellenspiel.de/*
-// @version     0.1.22
+// @version     0.1.23
 // @author      Gummibeer
 // @license     MIT
 // @run-at      document-end
 // @grant       GM_getResourceText
 // @resource    VehicleMap https://github.com/Gummibeer/lss_automator/raw/master/vehicle.map.json
 // @resource    MissionMap https://github.com/Gummibeer/lss_automator/raw/master/mission.map.json
+// @require     https://github.com/Gummibeer/lss_automator/raw/master/logger.js
 // ==/UserScript==
 
 (function () {
     const VEHICLE_MAP = JSON.parse(GM_getResourceText('VehicleMap'));
     const MISSION_MAP = JSON.parse(GM_getResourceText('MissionMap'));
+    const logger = new Logger('lss-automator', Logger.DEBUG);
 
     let starting_mission = false;
 
@@ -22,7 +24,7 @@
         return;
     }
 
-    console.info('init LSS-Automator');
+    logger.notice('initialize LSS-Automator');
 
     let $missions = $('#mission_list').find('div[mission_id]:not(.mission_deleted)').filter(function () {
         return $(this).find('.mission_panel_red').length === 1;
@@ -56,7 +58,7 @@
     }
 
     function handleMissionDelete(id) {
-        console.log('mission#' + id+' finished');
+        logger.info('mission#' + id+' finished');
     }
 
     function startMission(missionId, missionTypeId) {
@@ -72,7 +74,7 @@
             missionIntervalRuns++;
             if (missionIntervalRuns > 10) {
                 clearInterval(missionInterval);
-                console.error('mission#'+missionId+' not found');
+                logger.error('mission#'+missionId+' not found');
                 starting_mission = false;
                 return;
             }
@@ -85,7 +87,7 @@
             clearInterval(missionInterval);
 
             if ($mission.hasClass('mission_deleted') || $mission.find('.mission_panel_red').length === 0) {
-                console.error('mission#'+missionId+' already started');
+                logger.warning('mission#'+missionId+' already started');
                 starting_mission = false;
                 return;
             }
@@ -95,7 +97,7 @@
                 let timeout = parseInt($countdown.attr('timeleft'));
                 let startBefore = 1000 * 60 * 10;
                 if (timeout > (startBefore + (1000 * 60))) {
-                    console.log('mission#' + missionId + ' with countdown ignored');
+                    logger.warning('mission#' + missionId + ' with countdown ignored');
                     starting_mission = false;
                     return;
                 }
@@ -103,21 +105,21 @@
 
             let missionDetails = MISSION_MAP[missionTypeId];
             if (typeof missionDetails === 'undefined') {
-                console.error('mission#'+missionId+' details for type#' + missionTypeId + ' not found https://www.leitstellenspiel.de/einsaetze/' + missionTypeId);
+                logger.error('mission#'+missionId+' details for type#' + missionTypeId + ' not found https://www.leitstellenspiel.de/einsaetze/' + missionTypeId);
                 starting_mission = false;
                 return;
             }
 
             let missionVehicles = missionDetails.vehicles;
 
-            console.log('mission#' + missionId+' starting');
+            logger.info('mission#' + missionId+' starting');
 
             let buttonIntervalRuns = 0;
             let buttonInterval = setInterval(function () {
                 buttonIntervalRuns++;
                 if (buttonIntervalRuns > 10) {
                     clearInterval(buttonInterval);
-                    console.error('alarm button not found');
+                    logger.critical('alarm button not found');
                     starting_mission = false;
                     return;
                 }
@@ -135,7 +137,7 @@
                 let tableInterval = setInterval(function () {
                     tableIntervalRuns++;
                     if (tableIntervalRuns > 10) {
-                        console.error('vehicle table not found');
+                        logger.critical('vehicle table not found');
                         starting_mission = false;
                         clearInterval(tableInterval);
                         return;
@@ -149,7 +151,7 @@
                     let $tab = $('.tab-content .tab-pane.active', $iframe.contents());
                     let $alert = $tab.find('.alert');
                     if ($alert.length === 1) {
-                        console.error('mission#'+missionId+' no vehicles available');
+                        logger.error('mission#'+missionId+' no vehicles available');
                         setTimeout(startMission, 1000 * 60, missionId, missionTypeId);
                         $('#lightbox_box button#lightbox_close').trigger('click');
                         starting_mission = false;
@@ -167,9 +169,10 @@
                     for (let j = 0; j < Object.keys(missionVehicles).length; j++) {
                         let vehicleType = Object.keys(missionVehicles)[j];
                         let vehicleCount = missionVehicles[vehicleType];
-                        console.debug('mission#' + missionId + ' require ' + vehicleCount + ' ' + vehicleType);
+                        logger.debug('mission#' + missionId + ' require ' + vehicleCount + ' ' + vehicleType);
 
                         for (let i = 0; i < vehicleCount; i++) {
+                            // $table.find('tbody').find('tr').find('input[type=checkbox][wasser_amount]')
                             let $trs = $table.find('tbody').find('tr').filter(function () {
                                 let $tr = $(this);
 
@@ -181,7 +184,7 @@
                             });
 
                             if ($trs.length === 0) {
-                                console.error('mission#'+missionId+' not enough vehicles - missing: ' + vehicleType);
+                                logger.error('mission#'+missionId+' not enough vehicles - missing: ' + vehicleType);
                                 setTimeout(startMission, 1000 * 60, missionId, missionTypeId);
                                 $('#lightbox_box button#lightbox_close').trigger('click');
                                 starting_mission = false;
