9f17793c5d8a4fd5b322607f68c3a1431f6d4dad
Gummibeer <dev.gummibeer@gmail.com>
2019-05-08 09:21:08 +0200

    add wait for element promise

---
+++ wait_for_element.js
@@ -0,0 +1,26 @@
+function waitForElement(selector, $context, maxRuns) {
+    $context = typeof context === 'undefined' ? $(document) : context;
+    maxRuns = typeof maxRuns === 'undefined' ? 10 : maxRuns;
+
+    return new Promise(function (resolve, reject) {
+        let runs = 0;
+
+        let interval = setInterval(function () {
+            runs++;
+            if (runs > maxRuns) {
+                clearInterval(interval);
+                reject(new Error('element not found after ' + maxRuns + ' runs'));
+                return;
+            }
+
+            let $el = $(selector, $context);
+            if ($el.length === 0) {
+                return;
+            }
+
+            clearInterval(interval);
+
+            resolve($el);
+        }, 500);
+    });
+}
